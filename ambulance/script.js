545c52bd276bad60a208c10a7620297c4b73a3ea
dianhutagalung <63131758+dianhutagalung@users.noreply.github.com>
2020-11-03 16:54:09 +0700

    Add files via upload

---
+++ ambulance/script.js
@@ -0,0 +1,20 @@
+let myIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1;
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 10000);
+}
+
+function tabPeti() {
+    window.open("http://127.0.0.1:5500/peti.html");
+}
